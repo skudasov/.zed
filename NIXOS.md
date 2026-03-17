d0431ab22404acb564562f72a4af36801ec4f776
skudasov <f4hrenh9it@gmail.com>
2026-03-18 00:34:32 +0100

    fix

--- NIXOS.md
+++
@@ -1,9 +0,0 @@
-# Guide to setup NixOS VM
-
-1. Create a new `unstable` channel VM in OrbStack, name it `nixos`
-
-2. Configure it 
-```bash
-ssh nixos@orb
-
-```
--- nixos-vm/configuration.nix
+++ nixos-vm/configuration.nix
@@ -1,11 +1,16 @@
-{ modulesPath, ... }: {
-  imports = [ (modulesPath + "/profiles/qemu-guest.nix") ];
+{ modulesPath, pkgs, ... }: {
+  imports = [
+    (modulesPath + "/profiles/qemu-guest.nix")
+    ./hardware-configuration.nix
+  ];
 
   services.openssh.enable = true;
 
   users.users.root.openssh.authorizedKeys.keys = [
     "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIAdJWz1uvhoJS6QSeARRoDR+MFQE2w+Mgw+qVGdprXf+ f4hrenh9it@gmail.com"
   ];
+
+  environment.systemPackages = [ pkgs.jq ];
 
   networking.hostName = "nixos-target";
   system.stateVersion = "25.05";
